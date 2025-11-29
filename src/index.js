69069ed7e49ad7209bd4ee1138206cfbbfba65c8
PranavPatil23 <pranavmpatil2002@gmail.com>
2025-11-29 12:55:50 +0530

    utils created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import dotenv from 'dotenv';
 import connetDB from './db/index.js';
+import {app} from './app.js'
+
 
 dotenv.config({
     path:'./.env'
 })
 
-connetDB();
+connetDB()
+.then(() =>{
+    app.on("error",(error) =>{
+        console.log(`ERROR ${error}`);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000,() =>{
+        console.log(`Server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((err) =>{
+    console.log(`mongoDB connection failed !!! ${err}`)
+})
 
 
 
@@ -29,7 +43,7 @@ const app = express();
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
 
-        // application.on("error",(error) =>{ //this block of code is for when application is not able to working or have some problem to connet to DB as DB connetion is working.
+        // app.on("error",(error) =>{ //this block of code is for when application is not able to working or have some problem to connet to DB as DB connetion is working.
         //     console.log("Error",error);
         //     throw error;
             
